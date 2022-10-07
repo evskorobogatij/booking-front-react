3f9af498929a47bac096294f0d0a52a375b73197
Скоробогатый Евгений <evskorobogatij@gmail.com>
2022-10-07 12:54:34 +0300

    Локализация подразделений

--- src/modules/Department/DepartmentsPage.tsx
+++ src/modules/Department/DepartmentsPage.tsx
@@ -3,12 +3,14 @@ import DepartmentsList from './DepartmentsList'
 import EntityPage from '../../components/layouts/EntityPage'
 import { useCreateDepartmentMutation } from '../../services'
 import DepartmentForm from './DepartmentForm'
+import { useTranslation } from 'react-i18next'
 
 const DepartmentsPage: React.FC = () => {
+  const { t } = useTranslation()
   return (
     <EntityPage
-      title="Departments"
-      createBtnTitle="New department"
+      title={t('Departments')}
+      createBtnTitle={t('New department')}
       form={DepartmentForm}
       mutation={useCreateDepartmentMutation}
       container={<DepartmentsList />}
